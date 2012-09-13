2fe8430b780474b57b6b7043bff9088f117406dd
Neville Newey <nn@milo.com>
2012-09-13 14:58:41 -0700

    Added simple compile and run in sbcl tester

---
+++ bin/test_sbcl.sh
@@ -0,0 +1,9 @@
+
+for f in test_cases/*py;
+do
+		echo "=============================================="
+    echo "Compiling and running $f";
+		echo "=============================================="
+		python3 compile.py $f > /tmp/`basename $f .py`.lisp
+		sbcl --script /tmp/`basename $f .py`.lisp
+done;
