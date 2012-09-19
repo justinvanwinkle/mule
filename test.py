24e1f498d7c3175aeae7e9f1d7f2b63366fe4f98
Justin Van Winkle <justin@milo.com>
2012-09-19 03:53:46 -0700

    Revert "not quite working compile.lisp that uses cl-python's parser"
    This reverts commit cb512cbe98338a2b70c47c5324e7c253e8e93141.

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-x = {}
-x['test'] = 'yep'
 
-# def this_is_a_test(x, y='blah'):
-#     print("output")
-#     x = y
-#     return True
+
+def this_is_a_test(x, y='blah'):
+    print("output")
+    x = y
+    return True
