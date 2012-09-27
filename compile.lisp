9aac9b1db2f9040d5a8d3db57fce6893a9613b89
Justin Van Winkle <justin@milo.com>
2012-09-26 21:15:28 -0700

    yay, functions working

--- compile.lisp
+++ compile.lisp
@@ -3,6 +3,14 @@
 
 (asdf:operate 'asdf:load-op 'clpython)
 
+;; (MODULE-STMT
+;;   (SUITE-STMT
+;;    ((ASSIGN-STMT
+;;      (BINARY-EXPR #1=# (LITERAL-EXPR :NUMBER 1) (LITERAL-EXPR :NUMBER 1))
+;;      ((IDENTIFIER-EXPR CLPYTHON.USER::|x|)))
+;;     (ASSIGN-STMT
+;;      (BINARY-EXPR #1# (LITERAL-EXPR :NUMBER 2) (LITERAL-EXPR :NUMBER 2))
+;;      ((IDENTIFIER-EXPR CLPYTHON.USER::|y|))))))
 
 (defpackage :pycl.compile
   (:use
@@ -11,42 +19,57 @@
    :clpython.ast.token
    :clpython.user))
 
+(defvar dispatch-table (make-hash-table))
 
-(defun swap-symbol (s)
-  (case s
-    ((clpython.ast.operator:+) 'plus)
-    (otherwise s)))
+(defun id (node)
+  node)
 
+(defun visit (node)
+  (let* ((func (gethash (car node) dispatch-table 'id))
+         (result (funcall func node)))
+    (format t "using: ~a to visit ~%~a~%giving~%~a~%" func node result)
+    result))
 
-(defun translate (tree)
-  (when (not (null tree))
-    (if (listp (car tree))
-        (translate (car tree))
-      (setf (car tree) (swap-symbol (car tree))))
-    (translate (cdr tree)))
-  tree)
+(defun module-v (node)
+  (visit (second node)))
+(setf (gethash 'clpython.ast.node::|module-stmt| dispatch-table) 'module-v)
 
-(defmacro |module-stmt| (&body body)
-  (car body))
+(defun suite-eat (nodes)
+  (if (null nodes)
+      nil
+      (let ((current (car nodes))
+            (rest (cdr nodes)))
+        (if (eql (car current) 'clpython.ast.node::|assign-stmt|)
+            (assign-v current (suite-eat rest))
+            (list 'progn (visit current) (suite-eat rest))))))
 
-(defmacro |suite-stmt| (&rest (body))
-  `(progn ,@body))
+(defun suite-v (node)
+  (suite-eat (second node)))
+(setf (gethash 'clpython.ast.node::|suite-stmt| dispatch-table) 'suite-v)
 
-(defmacro |assign-stmt| (&rest body)
-  `(destructuring-bind ,(mapcar #'second (second body)) (list ,(car body))))
+(defun assign-v (node body)
+  (let ((assign-form (third node))
+        (assign-expr (second node)))
+    (if (eql (length assign-form) 1)
+        (list 'let (list (second assign-form) (visit assign-expr)) body)
+        (error "multiple assignment"))))
+(setf (gethash 'clpython.ast.node::|assign-stmt| dispatch-table) 'assign-v)
 
-(defmacro |binary-expr| (&rest body)
-  body)
+(defun binary-v (node)
+  (let ((op (second node))
+        (x (third node))
+        (y (fourth node)))
+    (list (gethash op dispatch-table) (visit x) (visit y))))
+(setf (gethash 'clpython.ast.node::|binary-expr| dispatch-table) 'binary-v)
+(setf (gethash 'clpython.ast.operator:+ dispatch-table) '+)
 
-(defmacro |literal-expr| (&rest body)
-  (when (eql :number (car body))
-    (second body)))
+(defun literal-v (node)
+  (third node))
+(setf (gethash 'clpython.ast.token::|literal-expr| dispatch-table) 'literal-v)
 
-(defmacro |identifier-expr| (&rest body)
-  (car body))
-
-(defmacro plus (&rest body)
-  `(+ ,@body))
+(defun identifier-v (node)
+  (
+(setf (gethash 'CLPYTHON.AST.NODE:|identifier-expr| dispatch-table) 'identifier-v)
 
 (defun pycl (filename)
   (let ((tree (clpython.parser:parse
@@ -57,14 +80,6 @@
   (let ((tree (pycl filename)))
     (eval (translate tree))))
 
-(eval-pycl #p"/home/jvanwink/repos/pycl/test_cases/test_assign.py")
+;(eval-pycl #p"/home/jvanwink/repos/pycl/test_cases/test_assign.py")
 
-;(translate (pycl #p"/home/jvanwink/repos/pycl/test_cases/test_assign.py"))
-;; (MODULE-STMT
-;;   (SUITE-STMT
-;;    ((ASSIGN-STMT
-;;      (BINARY-EXPR #1=# (LITERAL-EXPR :NUMBER 1) (LITERAL-EXPR :NUMBER 1))
-;;      ((IDENTIFIER-EXPR CLPYTHON.USER::|x|)))
-;;     (ASSIGN-STMT
-;;      (BINARY-EXPR #1# (LITERAL-EXPR :NUMBER 2) (LITERAL-EXPR :NUMBER 2))
-;;      ((IDENTIFIER-EXPR CLPYTHON.USER::|y|))))))
+(visit (pycl #p"/home/jvanwink/repos/pycl/test_cases/test_assign.py"))
--- test_cases/test_assign.py
+++ test_cases/test_assign.py
@@ -1,2 +1,6 @@
 x = 1 + 1
 y = 2 + 1
+
+
+def foo(x, y):
+    return x + y
