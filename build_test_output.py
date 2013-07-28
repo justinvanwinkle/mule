09875bccf28132b528e71033f4d75b2a56f82ed9
Justin Van Winkle <justin@milo.com>
2013-07-28 15:13:17 -0700

    setting up better test harness

---
+++ build_test_output.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from glob import glob
+from os import mkdir
+from os.path import split as splitpath
+from os.path import splitext
+
+import pratt
+
+if __name__ == '__main__':
+    try:
+        mkdir('OUTPUT')
+    except OSError:
+        pass
+
+    pratt.debug = False
+    in_fns = glob('test_cases/*.py')
+    for in_fn in in_fns:
+        try:
+            output = pratt.parse_file(in_fn).cl()
+            in_fn_name = splitext(splitpath(in_fn)[1])[0]
+            output_fn = 'OUTPUT/' + in_fn_name + '.lisp'
+            with open(output_fn, 'w') as f:
+                f.write(output)
+        except Exception, e:
+            print in_fn, e
--- runtests.lisp
+++ runtests.lisp
@@ -1,17 +1,25 @@
-(load "compile.lisp")
 
-(defun file-string (path)
-  (with-open-file (stream path)
-    (let ((data (make-string (file-length stream))))
-      (read-sequence data stream)
-      data)))
 
-(dolist (fn (directory "test_cases/*.py"))
-  (let ((compiled (pycl-compile fn))
-        (lispcode (read-from-string (file-string
-                                     (make-pathname :directory (pathname-directory fn)
-                                                    :name (pathname-name fn)
-                                                    :type "lisp")))))
-    (if (equal compiled lispcode)
-        (format t "++++~a has passed~%" fn)
-        (format t "----~a has failed~%~a~%~%~a~%" fn compiled lispcode))))
+(defun read-file (filespec)
+  (with-open-file (source-file filespec)
+    (loop collect (read source-file))))
+
+(defun compare-files (fn1 fn2)
+  (let ((f1 (read-file fn1))
+        (f2 (read-file fn2)))
+    (if (equalp f1 f2)
+         (format nil "~A MATCHES ~A" fn1 fn2)
+         (progn
+           (format nil "~A FAIL ~A" fn1 fn2)
+           (pprint f1)
+           (print "**")
+           (pprint f2)))))
+
+
+(dolist (fn (directory "test_cases/*.lisp"))
+  (let ((fn1 (make-pathname :directory (pathname-directory fn)
+                            :name (pathname-name fn)
+                            :type "lisp"))
+        (fn2 (make-pathname :directory "OUTPUT/"
+                            :name (pathname-name fn))))
+    (compare-files fn1 fns)))
