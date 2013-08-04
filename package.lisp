1b88517e29146cd9a78c87ce918828c3cc4b9a9e
Justin Van Winkle <justin@milo.com>
2013-08-04 07:26:58 -0700

    getting build process sorted out

--- package.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :mule-system (:use :cl))
-(asdf:defsystem :mule
-  :serial t
-  :description "An experiment in syntax and langauge extension"
-  :author "Justin Van Winkle <justin.vanwinkle@gmail.com>"
-  :license "Specify license here"
-  :depends-on (:cl :asdf)
-  :components ((:file "package") (:file "hello-world-ltk")))
